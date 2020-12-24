9c2aac931553940a82b8e4f2efbb32c50d612ad0
Mark Cinali <mcinali@Marks-MBP.fios-router.home>
2020-12-24 17:38:17 -0500

    simple api logic to insert into/update DBs -- more business logic needed

--- routes/streams.js
+++ routes/streams.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {
   createStream,
+  inviteParticipantToStream,
+  joinStream,
+  leaveStream,
+  endStream,
 } = require('../services/streams')
 
 
@@ -9,6 +13,50 @@ const {
 router.post('/', async function (req, res) {
   try {
     const results = await createStream(req.body)
+    return res.send(results)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({error: ``})
+  }
+})
+
+// Send Invites to  Stream
+router.post('/invite', async function (req, res) {
+  try {
+    const results = await inviteParticipantToStream(req.body)
+    return res.send(results)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({error: ``})
+  }
+})
+
+// Join Stream
+router.post('/join', async function (req, res) {
+  try {
+    const results = await joinStream(req.body)
+    return res.send(results)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({error: ``})
+  }
+})
+
+// Leave Stream
+router.post('/leave', async function (req, res) {
+  try {
+    const results = await leaveStream(req.body.streamParticipantId)
+    return res.send(results)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json({error: ``})
+  }
+})
+
+// End Stream
+router.post('/end', async function (req, res) {
+  try {
+    const results = await endStream(req.body.streamId)
     return res.send(results)
   } catch (error) {
     console.error(error)
--- services/streams.js
+++ services/streams.js
@@ -19,6 +19,54 @@ async function createStream(streamInfo){
   }
 }
 
+// Create Stream
+async function inviteParticipantToStream(inviteInfo){
+  try {
+    // TO DO: Send invite email
+    const results = insertStreamInvitation(inviteInfo)
+    return results
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+// Join Stream
+async function joinStream(joinInfo){
+  try {
+    // TO DO: Check if user is allowed to join stream
+    const results = insertStreamParticipant(joinInfo)
+    return results
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+// Leave Stream
+async function leaveStream(streamParticipantId){
+  try {
+    // TO DO: Check if participant already left stream
+    const results = updateStreamParticipantEndTime(streamParticipantId)
+    return results
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+// End Stream
+async function endStream(streamId){
+  try {
+    // TO DO: Check if stream has already ended
+    const results = updateStreamEndTime(streamId)
+    return results
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 module.exports = {
   createStream,
+  inviteParticipantToStream,
+  joinStream,
+  leaveStream,
+  endStream,
 }
